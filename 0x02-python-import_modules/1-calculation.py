fb700744017e35b0be2d549acc2d000a2bf2bdf3
Josh-dev69 <ebubejoshua69@gmail.com>
2023-03-10 03:54:03 -0800

    Mandatory task done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-# Perform addition
-add_result = add(a, b)
+    # Perform addition
+    add_result = add(a, b)
 
-# Perform subtraction
-sub_result = sub(a, b)
+    # Perform subtraction
+    sub_result = sub(a, b)
 
-# Perform multiplication
-mul_result = mul(a, b)
+    # Perform multiplication
+    mul_result = mul(a, b)
 
-# Perform division
-div_result = div(a, b)
+    # Perform division
+    div_result = div(a, b)
 
-# Printing the Results
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+    # Printing the Results
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-no_of_args = len(sys.argv) - 1
-Sum_of_argv = 0
+    no_of_args = len(sys.argv) - 1
+    Sum_of_argv = 0
 
-for i in range(no_of_args):
-    Sum_of_argv += int(sys.argv[i + 1])
-print("{}".format(Sum_of_argv))
+    for i in range(no_of_args):
+        Sum_of_argv += int(sys.argv[i + 1])
+    print("{}".format(Sum_of_argv))
