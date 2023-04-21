a52b155b9b874227f3ad14fb22eb5f98f743615e
Josh-dev69 <ebubejoshua69@gmail.com>
2023-04-20 19:17:34 -0700

    Added a test file

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,91 +1,71 @@
 #!/usr/bin/python3
-"""Defines unittests for Rectangle class."""
+
+"""Unit tests for Rectangle class"""
+
 import unittest
+
 from models.rectangle import Rectangle
 
-
 class TestRectangle(unittest.TestCase):
-    """Test Rectangle class."""
+    """Defines the unit test cases for the Rectangle class"""
 
-    def test_valid_init(self):
-        """Test for valid 'Rectangle.__init__()' initialization."""
-        r1 = Rectangle(1, 2)
-        self.assertEqual(r1.width, 1)
-        self.assertEqual(r1.height, 2)
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
-        self.assertIsNotNone(r1.id)
+    def test_valid_input(self):
+        """Tests the case when valid inputs are passed"""
+        r = Rectangle(10, 20)
+        self.assertEqual(r.width, 10)
+        self.assertEqual(r.height, 20)
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
+        self.assertEqual(r.id, 1)
 
-    def test_invalid_init(self):
-        """ Test for invalid 'Rectangle.__init__()' initialization """
-        with self.assertRaises(TypeError):
-            Rectangle("1", 2)
+    def test_invalid_width(self):
+        """Tests the case when an invalid width is passed"""
         with self.assertRaises(ValueError):
-            Rectangle(0, 2)
-        with self.assertRaises(TypeError):
-            Rectangle(1, 2, "3")
+            r = Rectangle(-10, 20)
+
+    def test_invalid_height(self):
+        """Tests the case when an invalid height is passed"""
         with self.assertRaises(ValueError):
-            Rectangle(1, 2, -3)
-        with self.assertRaises(TypeError):
-            Rectangle(1, 2, 3, "4")
+            r = Rectangle(10, -20)
+
+    def test_invalid_x(self):
+        """Tests the case when an invalid x value is passed"""
         with self.assertRaises(ValueError):
-            Rectangle(1, 2, 3, -4)
-        with self.assertRaises(TypeError):
-            Rectangle(1, 2, 3, 4, "5")
+            r = Rectangle(10, 20, -5)
+
+    def test_invalid_y(self):
+        """Tests the case when an invalid y value is passed"""
+        with self.assertRaises(ValueError):
+            r = Rectangle(10, 20, 5, -10)
 
     def test_area(self):
-        """Test Rectangle.area()."""
-        r1 = Rectangle(3, 5)
-        self.assertEqual(r1.area(), 15)
+        """Tests the area method"""
+        r = Rectangle(10, 20)
+        self.assertEqual(r.area(), 200)
 
     def test_display(self):
-        """Test Rectangle.display()."""
-        r1 = Rectangle(2, 3)
-        r1.display()
-        self.assertEqual(r1.display(), None)
+        """Tests the display method"""
+        r = Rectangle(3, 2)
+        expected_output = "###\n###\n"
+        with self.assertLogs() as cm:
+            r.display()
+        self.assertEqual(cm.output, [expected_output])
 
-    def test_update_args(self):
-        """Test for *args"""
-        r1 = Rectangle(10, 10, 10, 10)
-        r1.update(89)
-        self.assertEqual(r1.id, 89)
-        r1.update(89, 2)
-        self.assertEqual(r1.width, 2)
-        r1.update(89, 2, 3)
-        self.assertEqual(r1.height, 3)
-        r1.update(89, 2, 3, 4)
-        self.assertEqual(r1.x, 4)
-        r1.update(89, 2, 3, 4, 5)
-        self.assertEqual(r1.y, 5)
-
-    def test_update_kwargs(self):
-        """ Test for **kwargs """
-        r1.update(id=89)
-        self.assertEqual(r1.id, 89)
-        r1.update(width=2)
-        self.assertEqual(r1.width, 2)
-        r1.update(height=3)
-        self.assertEqual(r1.height, 3)
-        r1.update(x=4)
-        self.assertEqual(r1.x, 4)
-        r1.update(y=5)
-        self.assertEqual(r1.y, 5)
-        r1.update(width=1, x=2)
-        self.assertEqual(r1.width, 1)
-        self.assertEqual(r1.x, 2)
-
-    def test_to_dictionary(self):
-        """Test Rectangle.to_dictionary()."""
-        r1 = Rectangle(10, 2, 1, 9)
-        r1_dict = r1.to_dictionary()
-        expected = {'x': 1, 'y': 9, 'id': r1.id, 'height': 2, 'width': 10}
-        self.assertEqual(r1_dict, expected)
+    def test_update(self):
+        """Tests the update method"""
+        r = Rectangle(10, 20)
+        r.update(2, 5, 6)
+        self.assertEqual(r.id, 2)
+        self.assertEqual(r.width, 5)
+        self.assertEqual(r.height, 6)
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
 
     def test_str(self):
-        """Test Rectangle.__str__()."""
-        r1 = Rectangle(4, 6, 2, 1, 12)
-        self.assertEqual(str(r1), "[Rectangle] (12) 2/1 - 4/6)")
+        """Tests the str method"""
+        r = Rectangle(10, 20, 5, 6, 7)
+        expected_output = "[Rectangle] (7) 5/6 - 10/20"
+        self.assertEqual(str(r), expected_output)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
