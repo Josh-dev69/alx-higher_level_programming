70c3746184eb06d9d996f962ddbe3cc5ab942dbf
Joshua <85683603+Josh-dev69@users.noreply.github.com>
2023-06-14 18:58:06 +0100

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-	console.log('undefined is ' + process.argv[2]);
-} else if (process.argv[3] === undefined) {
-	console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else {
-	console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
