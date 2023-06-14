c81b5c1b30aec37d5ac1e1d784861e9da1deb023
Josh-dev69 <ebubejoshua69@gmail.com>
2023-06-14 15:40:38 -0700

    JavaScript Basics

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+exports.addMeMaybe =function (num, newFunt) {
+  newFunct(num + 1);
+};
---
+++ 0x12-javascript-warm_up/103-objects.fct.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const nyObject = {
+	type: 'object';
+	value: 12;
+};
+console.log(myObject):
+myObject.incr function () {
+  myObject.value +=1;
+};
+myObject.incr();
+console.log(myObject);#!/usr/bin/node
+myObject.incr();
+console.log(myObject);#!/usr/bin/node
+myObject.incr();
+console.log(myObject);#!/usr/bin/node
