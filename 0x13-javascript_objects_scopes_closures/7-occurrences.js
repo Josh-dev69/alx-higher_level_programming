8d088e388c2917509f6c98aa850a0391142579e3
Josh-dev69 <ebubejoshua69@gmail.com>
2023-06-15 12:33:45 -0700

    JS Objects

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let occurrence = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      occurrence++;
+    }
+  }
+  return occurrence;
+};
