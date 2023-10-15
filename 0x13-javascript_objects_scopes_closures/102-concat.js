e4292851c3ac1028736cad82099fb163f4a0a850
Joshua <85683603+Josh-dev69@users.noreply.github.com>
2023-10-15 19:39:45 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const file = require('fs');
-const fileA = process.argv[2];
-const textA = file.readFileSync(fileA, 'utf8');
-const fileB = process.argv[3];
-const textB = file.readFileSync(fileB, 'utf8');
-const fileC = process.argv[4];
-file.writeFileSync(fileC, textA + textB);
