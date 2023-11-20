91384edf03c90e30978cb8752bc10fc421ce8909
root <root@joshua.myguest.virtualbox.org>
2023-11-20 07:38:40 -0800

    List all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,13 @@ import sys
 
 if __name__ == "__main__":
     # Connect to the database
-    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    conn = MySQLdb.connect(
+            host = "localhost",
+            port = 3306,
+            user = sys.argv[1],
+            passwd = sys.argv[2],
+            db = sys.argv[3]
+        )
     cursor = conn.cursor()
 
     # Execute the SQL Query
