dd0d51df5f705748cdef1c1e7bb4a632ef39070c
Josh-dev69 <ebubejoshua69@gmail.com>
2023-11-21 07:30:04 +0100

    Updated task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-""" A Script that takes an argument and match it with
-    the values in the 'states' table of the
-    database 'hbtn_0e_0_usa' """
+""" 
+
+A Script that takes an argument and match it with
+the values in the 'states' table of the
+database 'hbtn_0e_0_usa' 
+
+"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Set up connection
     conn = MySQLdb.connect(
-            user = sys.argv[1],
-            passwd = sys.argv[2],
-            db = sys.argv[3]
-        )
-    cursor = conn.cursor()
+        host="localhost",
+        port=3306,
+        user = sys.argv[1],
+        passwd = sys.argv[2],
+        db = sys.argv[3]
+    )
+    cur = conn.cursor()
 
-    query = "SELECT * FROM states WHERE name = %s \
-            ORDER BY id ASC"
-    cursor.execute(query, (sys.argv[4],))
+    query = "SELECT * FROM states WHERE name LIKE BINARY'{}' \
+            ORDER BY id ASC".format(sys.argv[4])
+    cur.execute(query)
 
-    results = cursor.fetchall()
+    results = cur.fetchall()
     for row in results:
         print(row)
+    cur.close()
+    conn.close()
