b957f78d7e76fb50d658a236ace190e46e29f7e3
Josh-dev69 <ebubejoshua69@gmail.com>
2023-11-21 06:59:38 +0100

    Updated Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         charset="utf8"
     )
     cur = conn.cursor()
