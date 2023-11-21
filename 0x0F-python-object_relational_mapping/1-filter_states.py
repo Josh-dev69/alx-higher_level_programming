39ae071fe9b8758324972df82699f51e08fc07a1
Josh-dev69 <ebubejoshua69@gmail.com>
2023-11-21 07:15:04 +0100

    Updated Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         port=3306,
         user = sys.argv[1],
         passwd = sys.argv[2],
-        db = sys.argv[3]
+        db = sys.argv[3],
         charset="utf8"
     )
     cursor = conn.cursor()
