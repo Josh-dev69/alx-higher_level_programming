bd10f6c55865fd7297171bccd63d8118b96c2b3c
Josh-dev69 <ebubejoshua69@gmail.com>
2023-11-24 05:12:54 +0100

    Update

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,16 +9,24 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+#!/usr/bin/python3
+"""
+This class script a delete a state
+in database using ORM
+
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 if __name__ == "__main__":
     """
     Access the database and list the states
     """
-    user = argv[1]
-    pwd = argv[2]
-    db = argv[3]
-    
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-            .format(user, pwd, db), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
