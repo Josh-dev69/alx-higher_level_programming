00c87fb86351eb75f576b9cccafc5fe247a26b20
Josh-dev69 <ebubejoshua69@gmail.com>
2023-11-24 06:14:48 +0100

    Tasks done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
 """
-    A script that lists all cities from the
-    database hbtn_0e_4_usa
+    A script that lists all cities from the database 
+    hbtn_0e_4_usa
 """
+
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(
         host="localhost",
-        port=3306,
         user=argv[1],
         passwd=argv[2],
-        db=argv[3],
-        charset="utf8"
+        db=argv[3]
     )
     cur = conn.cursor()
-    query = "SELECT cities.name FROM cities \
+    statement = ''' SELECT cities.name FROM cities \
             JOIN states ON cities.state_id = states.id \
             WHERE states.name LIKE BINARY %s \
-            ORDER BY cities.id ASC"
-    cur.execute(query, (argv[4], ))
-    result = cur.fetchall()
-    if result is not None:
-        result_strings = []
-        for row in result:
-            result_strings.append(str(row[0]))
+            ORDER BY cities.id ASC
+            '''
 
-    result_string = ", ".join(result_strings)
-    print(result_string)
-
+    cur.execute(statement, (argv[4], ))
+    rows = cur.fetchall()
+    if rows is not None:
+        print(", ".join([row[0] for row in rows]))
     cur.close()
     conn.close()
--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,30 +9,15 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-#!/usr/bin/python3
-"""
-This class script a delete a state
-in database using ORM
-
-"""
-
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 if __name__ == "__main__":
     """
-    Access the database and list the states
+    Access the database and listing the state
     """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
-
-    session.close()
+    session.close
