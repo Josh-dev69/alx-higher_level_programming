915f3871ec1fb345246bbec73cd168ae1b03b0b3
Josh-dev69 <ebubejoshua69@gmail.com>
2024-01-03 21:28:44 +0100

    Request Store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,38 +1,35 @@
 #!/usr/bin/node
 /**
- * Script to get contents of a web page and save it to a file.
+ * script to get contents of web and save in a file
  */
 
 const request = require('request');
 const fs = require('fs');
 
-// Check if the correct number of arguments is provided
-if (process.argv.length !== 4) {
-  console.error('Usage: ./script.js <URL> <file-path>');
-  process.exit(1);
-}
-
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-// Make a GET request to the specified URL
-request(url, function (error, response, body) {
+function getData (url, callback) {
+  request(url, function (error, response, body) {
+    if (error) {
+      callback(error);
+      return;
+    }
+    if (response.statusCode === 200) {
+      callback(null, body);
+    }
+  });
+}
+
+getData(url, function (error, body) {
   if (error) {
     console.error(error);
     return;
   }
 
-  if (response.statusCode === 200) {
-    // Write data to the file
-    fs.writeFile(filePath, body, function (err) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`Data successfully saved to ${filePath}`);
-      }
-    });
-  } else {
-    console.error(`Unexpected status code: ${response.statusCode}`);
-  }
+  fs.writeFile(filePath, body, function (err) {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
-
