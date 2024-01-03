e5c3337451e70a2f1e557d149b89c3f2e9bbc627
Josh-dev69 <ebubejoshua69@gmail.com>
2024-01-03 22:18:15 +0100

    writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 /**
- *
  * Write to a file via the content of a command line
  * argument
  *
-*/ 
+ */ 
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
