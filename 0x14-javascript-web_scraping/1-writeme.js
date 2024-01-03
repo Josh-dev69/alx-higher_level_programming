cacbae42f93a8a170dc228778a511ff942fdb080
Josh-dev69 <ebubejoshua69@gmail.com>
2024-01-03 22:14:55 +0100

    writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,15 +7,12 @@
 */ 
 const fs = require('fs');
 
-// Extract the file path and content from command line arguments
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
-// Write the content to the file in utf-8
 fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
   }
 });
 
