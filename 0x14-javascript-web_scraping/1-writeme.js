90864b07cbd70657a592acb3057f97a1783ab10c
Josh-dev69 <ebubejoshua69@gmail.com>
2024-01-03 22:17:03 +0100

    writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,10 +7,7 @@
 */ 
 const fs = require('fs');
 
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
   if (err) {
     console.log(err);
   }
